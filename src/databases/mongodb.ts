fe3804c23eaec6643a23f30378ece06ca0cb322b
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-08 01:06:20 -0500

    mongo ts

---
+++ src/databases/mongodb.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+
+import { config } from '../config/index';
+
+export const dbConnection = async () => {
+  try {
+    await mongoose.connect(config.mongoConnect, {
+      useNewUrlParser    : true,
+      useUnifiedTopology : true,
+      useCreateIndex     : true,
+    });
+  } catch (error) {
+    throw new Error('Algo salio mal en la coneccion a mongo');
+  }
+};
+
+// module.exports = {
+//   dbConnection,
+// };
