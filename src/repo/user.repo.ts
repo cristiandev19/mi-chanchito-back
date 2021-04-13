1fbc45768ced02aad5e4ceb83025a706f2b12610
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-12 23:42:54 -0500

    anything

--- src/repo/user.repo.ts
+++ src/repo/user.repo.ts
@@ -1,9 +1,35 @@
-import { IUsers } from "models/Users";
-import { Repo } from "./base.repo";
+import { IUsers, Users } from "models/Users";
+import { Repo } from "../core/infra/Repo";
 
-export interface IUsersRepo extends Repo<IUsers> {
+interface IUsersRepo extends Repo<IUsers> {
   getUserById(userId: string): Promise<IUsers>;
-  saveUser(user: IUsers): Promise<IUsers>;
   // findAllVinylByArtistName(artistName: string): Promise<TransfersCollection>;
   // getVinylOwnedByUserId(userId: string): Promise<TransfersCollection>;
+}
+
+export class UsersRepo implements IUsersRepo {
+  constructor() {}
+
+  public getUserById(userId: string) {
+    return new Promise<IUsers>((resolve) => {
+      const user: IUsers = {
+        email: '',
+        emailVerified: 'false',
+        password: '',
+        passwordResetToken: '',
+        profile: {
+          lastNames: '',
+          names: '',
+          picture: ''
+        },
+      };
+      return resolve(user);
+    });
+  }
+
+  public exists(user: IUsers) {
+    return new Promise<boolean>((resolve) => {
+      return resolve(true);
+    });
+  }
 }
