96d05a1ddaefcb24fb386dec7ef6d8207ea81a60
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-13 01:00:49 -0500

    nueva carpeta de midlewares y errores

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,19 @@
+const { config } = require('../../config');
+
+export function withErrorStack(error, stack) {
+  if (config.dev) {
+    return { error, stack };
+  }
+  return error;
+}
+
+export function logErrors(err, req, res, next) {
+  console.log(err);
+  next(err);
+}
+
+export function errorHandler(err, req, res, next) { // eslint-disable-line
+  res.status(err.status || 500);
+  res.json(withErrorStack(err.message, err.stack));
+}
+
