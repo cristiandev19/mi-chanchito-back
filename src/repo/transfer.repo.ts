e07ae1c266e25f35fd13a7ccc2f4923405776c1a
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-13 20:25:49 -0500

    deleting

--- src/repo/transfer.repo.ts
+++
@@ -1,6 +0,0 @@
-import { ITransfers, Transfers } from "models/Transfers";
-import { Repo } from "../core/infra/Repo";
-
-export interface ITransferlRepo extends Repo<ITransfers> {
-  getTransferById(vinylId: string): Promise<ITransfers>;
-};
