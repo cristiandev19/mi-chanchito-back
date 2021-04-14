8888baea63badfbb9adf2adf837897bc525ebf90
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-13 20:26:21 -0500

    transfer model

---
+++ src/modules/transfer/transfer.model.ts
@@ -0,0 +1,18 @@
+import * as mongoose from 'mongoose';
+import { typeCashFlow } from '../../constants/transfer';
+import { ITransfers } from './transfer.interface';
+
+const transfesSchema = new mongoose.Schema<ITransfers>({
+  title        : { type: String, default: '', required: true },
+  description  : { type: String, default: '', required: false },
+  amount       : { type: Number, default: 0, required: true },
+  dateTransfer : { type: Date, default: Date.now, required: true },
+  cashFlow     : {
+    type     : String,
+    default  : '',
+    required : true,
+    enum     : [...Object.values(typeCashFlow)],
+  }
+});
+
+export const Transfers = mongoose.model('Transfers', transfesSchema);
