b7bce9e95ff3d49375b4169770cf71c7b285911d
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-16 01:24:59 -0500

    fixing importing errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,25 @@ module.exports = {
     node   : true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:import/typescript'
   ],
   parser        : '@typescript-eslint/parser',
   parserOptions : {
     ecmaVersion : 12,
     sourceType  : 'module',
+  },
+  // settings: {
+  //   'import/resolver': {
+  //     node: {
+  //       extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  //       // moduleDirectory : ['node_modules', 'src/'],
+  //     },
+  //   },
+  // },
+  'settings': {
+    'import/no-unresolved': 0, // Turn off 'Unable to resolve path to module ...' error
+    'import/extensions': 0,
   },
   plugins: [
     '@typescript-eslint',
@@ -23,7 +36,10 @@ module.exports = {
         on          : 'colon',
       },
     }],
-    'consistent-return'    : ['off'],
-    'no-underscore-dangle' : ['off'],
+    'consistent-return'                 : ['off'],
+    'no-underscore-dangle'              : ['off'],
+    'no-unused-vars'                    : 'off',
+    '@typescript-eslint/no-unused-vars' : ['error'],
+    'class-methods-use-this'            : 'off'
   },
 };
