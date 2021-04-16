84432d1cd29021fb2499eba41970267d31563be9
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-16 01:26:49 -0500

    its oket

--- src/modules/transfer/transfer.model.ts
+++ src/modules/transfer/transfer.model.ts
@@ -1,10 +1,10 @@
 import * as mongoose from 'mongoose';
 import typeCashFlow from '../../constants/transfer';
-import { ITransfers } from './transfer.interface';
+import { ITransfers } from '../transfer/transfer.interface';
 
-const transfesSchema = new mongoose.Schema<ITransfers>({
-  title        : { type: String, default: '', required: true },
-  description  : { type: String, default: '', required: false },
+const transfesSchema = new mongoose.Schema<ITransfers & mongoose.Document>({
+  description  : { type: String, default: '', required: true },
+  details      : { type: String, default: '', required: false },
   amount       : { type: Number, default: 0, required: true },
   dateTransfer : { type: Date, default: Date.now, required: true },
   cashFlow     : {
