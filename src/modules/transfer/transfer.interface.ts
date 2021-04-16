10905708b2ad3dacec48b33e67a780a24c18ee8b
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-16 01:26:33 -0500

    yep

--- src/modules/transfer/transfer.interface.ts
+++ src/modules/transfer/transfer.interface.ts
@@ -1,6 +1,9 @@
-export interface ITransfers {
-  title : String;
+import { Document } from 'mongoose';
+
+export interface ITransfers extends Document {
+  _id: String;
   description : String;
+  details: String;
   amount : Number;
   dateTransfer : Date;
   cashFlow : String;
