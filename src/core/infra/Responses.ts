b8355921f290767c1b3e9a81abe3ff0ddcaf2b6d
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-16 22:36:45 -0500

    esta vaina fue bien complicada

--- src/core/infra/Responses.ts
+++ src/core/infra/Responses.ts
@@ -1,8 +1,30 @@
-export interface IResponseSuccess<T> {
-  success: boolean,
-  payload: T
+interface IResponseSuccess {
+  success: boolean;
+  error?: never;
 }
 
-export interface IResponseFail {
-  error: Error,
+interface IResponseFail {
+  success?: never;
+  error: Error;
 }
+
+// export type IResponse<T> = IResponseSuccess<T> | IResponseFail<T>;
+export type IResponse<T> = {
+  payload: T
+} & (
+      | IResponseSuccess
+      | IResponseFail
+  )
+// export type IResponse<T> = {
+//   payload: T | null
+// } & (
+//     { success: boolean } | { error: Error }
+//   )
+
+
+// type IResponse = {
+//   title: string;
+// } & (
+//       | { component: object; click?: never }
+//       | { component?: never; click: boolean }
+//   )
