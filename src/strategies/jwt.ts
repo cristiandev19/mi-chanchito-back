6d611a2b38ad78cf81397c3cb355324066d34c8f
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-16 22:37:24 -0500

    hey you

--- src/strategies/jwt.ts
+++ src/strategies/jwt.ts
@@ -1,30 +1,24 @@
-// const JwtStrategy = require('passport-jwt').Strategy;
-// import passportJwt from 'passport-jwt';
 import { Strategy, ExtractJwt } from 'passport-jwt';
-// import passportJwt from 'passport-jwt';
-import { Users } from '../models/Users';
 
-
-
-import { config } from '../config/index';
+import config from '../config/index';
+import { UsersRepo } from 'modules/user/user.repo';
 
 const opts = {
   jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey    : config.authJwtSecret,                       // normally store this in process.env.secret
+  secretOrKey    : config.authJwtSecret, // normally store this in process.env.secret
 };
 
 // Aqui debemos cambiarlo para que llame a la BD por un id dentro del token
 // module.
-const jwtStrategy = new Strategy(opts, (jwtPayload, done) => {
-  Users.findOne({ _id: jwtPayload.sub }, (err, user) => {
-    if (err) {
-      return done(err);
-    }
-    if (!user) {
-      return done(null, false);
-    }
+const jwtStrategy = new Strategy(opts, async (jwtPayload, done) => {
+  try {
+    const usersRepo = new UsersRepo();
+    const user = await usersRepo.getUserById(jwtPayload.sub);
+    if (!user) return done(null, false);
     return done(null, user);
-  });
+  } catch (error) {
+    return done(error);
+  }
 });
 
 export {
