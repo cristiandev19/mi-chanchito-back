23b1c1f585606e102ec186a2999cf41aaff6ee97
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-20 01:16:25 -0500

    borrando cositas

--- server.ts
+++ server.ts
@@ -1,11 +1,6 @@
 import App from './app';
 import TransferController from './src/modules/transfer/transfer.controller';
 import AuthController from './src/modules/auth/auth.controlller';
-// import PostController from './post/post.controller';
-// import ReportController from './report/report.controller';
-// import UserController from './user/user.controller';
-// import validateEnv from './utils/validateEnv';
-// validateEnv();
 
 const app = new App(
   [
