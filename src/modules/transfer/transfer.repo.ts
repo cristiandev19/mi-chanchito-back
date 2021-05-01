7c41542956a87a51a586ffa47fa3122378912267
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-30 23:24:46 -0500

    update

--- src/modules/transfer/transfer.repo.ts
+++ src/modules/transfer/transfer.repo.ts
@@ -6,6 +6,7 @@ import { Repo } from "../../infrastructure/helpers/Repo";
 export interface ITransferRepo extends Repo<ITransfers> {
   getTransferById(transferId: string, userId: string): Promise<IResponse<ITransfers>>;
   getAllTransfers(userId: string): Promise<IResponse<ITransfers[]>>;
+  update(transfer: ITransfers): Promise<IResponse<ITransfers>>;
 };
 
 export class TransferRepo implements ITransferRepo {
@@ -29,10 +30,11 @@ export class TransferRepo implements ITransferRepo {
     });
   }
 
-  public delete(transfer: ITransfers) {
-    return new Promise<IResponse<ITransfers>>((resolve) => {
+  public delete(idTransfer: string) {
+    return new Promise<IResponse<ITransfers>>(async (resolve) => {
       try {
-        console.log('transfer._id', transfer._id);
+        const res = await Transfers.deleteOne({ _id: idTransfer });
+        console.log('res', res);
         return resolve({ success: true, payload: null });
       } catch (error) {
         return resolve({ error, payload: null });
@@ -79,6 +81,38 @@ export class TransferRepo implements ITransferRepo {
       }
     });
   }
+
+  public update(transfer: ITransfers) {
+    return new Promise<IResponse<ITransfers>>(async (resolve) => {
+      try {
+        console.log('transfer', transfer);
+        if (!transfer.userId) throw new Error('El usuario es necesario');
+        if (!transfer._id) throw new Error('Es obligatorio el id');
+        const result = await Transfers.updateOne({
+          _id: transfer._id,
+        }, { $set: {
+            description  : transfer.description,
+            details      : transfer.details,
+            amount       : transfer.amount,
+            dateTransfer : transfer.dateTransfer,
+            cashFlow     : transfer.cashFlow,
+        } });
+        // const newTransferObj = new Transfers({
+        //   userId       : transfer.userId,
+        // });
+        // newTransferObj.save({}, (err, newTransfer: ITransfers) => {
+        //   if (err) throw err;
+        //   console.log('transfer)', newTransfer);
+        //   return resolve({ success: true, payload: newTransfer });
+        // });
+        console.log('result', result);
+        return resolve({ success: true, payload: transfer });
+      } catch (error) {
+        return resolve({ error, payload: null });
+      }
+    });
+  }
+
 
   public getTransferById(transferId: string) {
     return new Promise<IResponse<ITransfers>>((resolve) => {
