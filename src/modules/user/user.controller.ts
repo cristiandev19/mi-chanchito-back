bf0cf6322a81f836559d03ee1d3709551bdb47c1
cristiandev <cristiansotomayor.dev@gmail.com>
2021-05-11 21:13:09 -0500

    fix

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -21,48 +21,21 @@ class UserController implements IController {
     // this.router.post(`${this.path}/create`, this.createTransfer);
   }
 
-  private getTransferById = async (request: Request, response: Response, next: NextFunction) => {
-    try {
-      const { id } = request.params;
-      console.log('id', id);
-      const transfer = await this.transferRepo.getTransferById(id);
-      return response.send({
-        status   : 200,
-        message  : 'hey',
-        response : transfer,
-      });
-    } catch (error) {
-      return next(error);
-    }
-  }
+  // private createTransfer = async (request: Request, response: Response, next: NextFunction) => {
+  //   try {
+  //     console.log('request', request.body);
+  //     // const transfer = await this.transferRepo.save({ ...request.body });
+  //     // console.log('transfer', transfer.error);
+  //     return response.send({
+  //       status   : 200,
+  //       message  : 'hey',
+  //       response : null,
+  //     });
+  //   } catch (error) {
+  //     return next(error);
+  //   }
+  // }
 
-  private getAllTransfers = async (request: Request, response: Response, next: NextFunction) => {
-    try {
-      // const transfers = await this.transferRepo.getAllTransfers();
-      return response.send({
-        status   : 200,
-        message  : 'hey',
-        response : null,
-      });
-    } catch (error) {
-      return next(error);
-    }
-  }
-
-  private createTransfer = async (request: Request, response: Response, next: NextFunction) => {
-    try {
-      console.log('request', request.body);
-      // const transfer = await this.transferRepo.save({ ...request.body });
-      // console.log('transfer', transfer.error);
-      return response.send({
-        status   : 200,
-        message  : 'hey',
-        response : null,
-      });
-    } catch (error) {
-      return next(error);
-    }
-  }
 }
 
 export default UserController;
