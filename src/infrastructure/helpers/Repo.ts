df488c7afbebc9df49eeb9cd4e6f5da5bb398262
cristiandev <cristiansotomayor.dev@gmail.com>
2021-05-19 21:38:30 -0500

    modifying repo

--- src/infrastructure/helpers/Repo.ts
+++ src/infrastructure/helpers/Repo.ts
@@ -3,5 +3,5 @@ import { IResponse } from "./Responses";
 export interface Repo<T> {
   exists(t: T): Promise<IResponse<boolean>>;
   delete(t: string): Promise<IResponse<T>>;
-  save(t: T): Promise<IResponse<T>>;
+  save(t: T): Promise<IResponse<T | void>>;
 }
